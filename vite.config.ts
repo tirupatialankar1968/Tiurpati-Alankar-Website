c4de28cf63ac3cee755c506b676dc3b70266ff38
tirupatialankar1968 <alankartirupati@gmail.com>
2025-05-03 17:02:36 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
